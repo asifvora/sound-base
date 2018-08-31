07f8d23a9f15e32634581300ef50e2681c0945cd
asif <asif.vora@multidots.com>
2018-08-31 15:22:04 +0530

    Update webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -27,16 +27,11 @@ module.exports = {
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: "file-loader?name=/src/assets/img/[name].[ext]"
+                loader: "file-loader",
             }
         ]
     },
     plugins: [htmlPlugin],
-    // Enable importing JS files without specifying their's extenstion
-    // So we can write:
-    // import MyComponent from './my-component';
-    // Instead of:
-    // import MyComponent from './my-component.jsx';
     resolve: {
         extensions: ['.js', '.jsx', '.css'],
     }
