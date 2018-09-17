df582c3e0a6263d40fc5e3d35fd8419dd48d67a8
asif <asif.vora@multidots.com>
2018-09-17 18:32:29 +0530

    logo path change

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import logo from '../assets/img/react.png';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={require('../assets/img/react.png')} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello World
+          Hello World {new Date().toString()}
         </p>
       </div>
     );
